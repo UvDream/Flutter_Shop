72065fb0e0ee2fb411e38105083725a7c2a7f946
wangzhongjie <uvdream@163.com>
2019-07-01 20:07:32 +0800

    formdata

--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -12,54 +12,40 @@ class IndexPage extends StatefulWidget {
 
 class _IndexPageState extends State<IndexPage> {
   // 底部导航栏
-  final List<BottomNavigationBarItem> bottomBars=[
+  final List<BottomNavigationBarItem> bottomBars = [
+    BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), title: Text('首页')),
     BottomNavigationBarItem(
-      icon: Icon(CupertinoIcons.home),
-      title: Text('首页')
-    ),
+        icon: Icon(CupertinoIcons.search), title: Text('分类')),
     BottomNavigationBarItem(
-      icon: Icon(CupertinoIcons.search),
-      title: Text('分类')
-    ),
+        icon: Icon(CupertinoIcons.shopping_cart), title: Text('购物车')),
     BottomNavigationBarItem(
-      icon: Icon(CupertinoIcons.shopping_cart),
-      title: Text('购物车')
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(CupertinoIcons.profile_circled),
-      title: Text('会员中心')
-    )
+        icon: Icon(CupertinoIcons.profile_circled), title: Text('会员中心'))
   ];
   // 对应页面
-  final List tabBodies=[
-    HomePage(),
-    CategoryPage(),
-    CarPage(),
-    MemberPage()
-  ];
-  int currentIndex=0;
+  final List tabBodies = [HomePage(), CategoryPage(), CarPage(), MemberPage()];
+  int currentIndex = 0;
   var currentPage;
   // 初始化
   @override
   void initState() {
-    currentPage=tabBodies[currentIndex];
+    currentPage = tabBodies[currentIndex];
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
       bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: currentIndex,
-        items: bottomBars,
-        onTap: ((index)=>{
-          setState((){
-            currentIndex=index;
-            currentPage=tabBodies[index];
-          })
-        })
-      ),
+          type: BottomNavigationBarType.fixed,
+          currentIndex: currentIndex,
+          items: bottomBars,
+          onTap: ((index) => {
+                setState(() {
+                  currentIndex = index;
+                  currentPage = tabBodies[index];
+                })
+              })),
       body: currentPage,
     );
   }
