8e1770e8f8adfc5341779efb5a0c4073e7642423
wangzhongjie <uvdream@163.com>
2019-07-03 21:22:23 +0800

    伪造请求头

---
+++ lib/config/httpHeaders.dart
@@ -0,0 +1,13 @@
+const httpHeaders={
+'Accept':'application/json, text/plain, */*',
+'Accept-Encoding':'gzip, deflate, br',
+'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7',
+'Connection': 'keep-alive',
+'Content-Length': '2',
+'Content-Type': 'application/json',
+'Cookie':'_ga=GA1.2.297393199.1561978379; GCID=06ffc56-0dceb5d-57118f2-406de13; GCESS=BAEEnVUYAAIEJeYZXQsCBAAIAQMEBAAvDQAHBAIpFh0MAQEFBAAAAAADBCXmGV0JAQEKBAAAAAAGBFQUtN8-; _gid=GA1.2.1890860695.1562158673; _gat=1; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1561978379,1561978406,1562158674; SERVERID=1fa1f330efedec1559b3abbcb6e30f50|1562158674|1562158673; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1562158685',
+'Host': 'time.geekbang.org',
+'Origin': 'https://time.geekbang.org',
+'Referer': 'https://time.geekbang.org/',
+'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+};
