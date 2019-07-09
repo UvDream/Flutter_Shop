97ded423031a3638e6a50a0a7b55d288cc228935
wangzhongjie <uvdream@163.com>
2019-07-09 15:46:07 +0800

    楼层拆分

---
+++ lib/pages/home/floor.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+// 楼层
+class FloorTitle extends StatelessWidget {
+  final String picture_address;
+  const FloorTitle({Key key, this.picture_address}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: Image.network(picture_address),
+    );
+  }
+}
+
+// 楼层商品列表
+class FloorContent extends StatelessWidget {
+  final List floorGoodsList;
+  const FloorContent({Key key, this.floorGoodsList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[_firstRow(), _otherGoods()],
+      ),
+    );
+  }
+
+  Widget _firstRow() {
+    return Row(
+      children: <Widget>[
+        _goodsItem(floorGoodsList[1]),
+        Column(
+          children: <Widget>[
+            _goodsItem(floorGoodsList[2]),
+            _goodsItem(floorGoodsList[3]),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _otherGoods() {
+    return Row(
+      children: <Widget>[
+        _goodsItem(floorGoodsList[3]),
+        _goodsItem(floorGoodsList[4])
+      ],
+    );
+  }
+
+  Widget _goodsItem(Map goods) {
+    return Container(
+      width: ScreenUtil().setWidth(375),
+      child: InkWell(
+        onTap: () {},
+        child: Image.network(goods['image']),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
 import 'dart:convert';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import './home/swiper.dart';
 import './home/top_navigator.dart';
 import './home/advertise.dart';
 import './home/leader.dart';
 import './home/product.dart';
+import './home/floor.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -93,68 +93,6 @@ class _HomePageState extends State<HomePage>
               }
             },
           )),
-    );
-  }
-}
-
-// 楼层
-class FloorTitle extends StatelessWidget {
-  final String picture_address;
-  const FloorTitle({Key key, this.picture_address}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(8.0),
-      child: Image.network(picture_address),
-    );
-  }
-}
-
-// 楼层商品列表
-class FloorContent extends StatelessWidget {
-  final List floorGoodsList;
-  const FloorContent({Key key, this.floorGoodsList}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: <Widget>[_firstRow(), _otherGoods()],
-      ),
-    );
-  }
-
-  Widget _firstRow() {
-    return Row(
-      children: <Widget>[
-        _goodsItem(floorGoodsList[1]),
-        Column(
-          children: <Widget>[
-            _goodsItem(floorGoodsList[2]),
-            _goodsItem(floorGoodsList[3]),
-          ],
-        )
-      ],
-    );
-  }
-
-  Widget _otherGoods() {
-    return Row(
-      children: <Widget>[
-        _goodsItem(floorGoodsList[3]),
-        _goodsItem(floorGoodsList[4])
-      ],
-    );
-  }
-
-  Widget _goodsItem(Map goods) {
-    return Container(
-      width: ScreenUtil().setWidth(375),
-      child: InkWell(
-        onTap: () {},
-        child: Image.network(goods['image']),
-      ),
     );
   }
 }
