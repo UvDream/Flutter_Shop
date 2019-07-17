26cb73e0d95ba5c0f9d3802b6787afb475f06747
wangzhongjie <uvdream@163.com>
2019-07-17 16:53:39 +0800

    [fix] 拆分代码

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -6,6 +6,7 @@ import '../model/category.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provide/provide.dart';
 import '../provide/child_category.dart';
+import './category/left_nav.dart';
 
 class CategoryPage extends StatefulWidget {
   @override
@@ -30,79 +31,6 @@ class _CategoryPageState extends State<CategoryPage> {
         ),
       ),
     );
-  }
-}
-
-// 左侧分类
-class LeftCatgegoryNav extends StatefulWidget {
-  @override
-  _LeftCatgegoryNavState createState() => _LeftCatgegoryNavState();
-}
-
-class _LeftCatgegoryNavState extends State<LeftCatgegoryNav> {
-  List list = [];
-  var listIndex = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _getCategory();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: ScreenUtil().setWidth(180),
-      decoration: BoxDecoration(
-        border: Border(right: BorderSide(width: 1, color: Colors.black12)),
-      ),
-      child: ListView.builder(
-        itemCount: list.length,
-        itemBuilder: (context, index) {
-          return _leftInkWell(index);
-        },
-      ),
-    );
-  }
-
-  // 左侧子项
-  Widget _leftInkWell(int index) {
-    bool isClick = false;
-    isClick = (index == listIndex) ? true : false;
-    return InkWell(
-      onTap: () {
-        setState(() {
-          listIndex = index;
-        });
-        var childList = list[index].bxMallSubDto;
-        Provide.value<ChildCategory>(context).getChildCategory(childList);
-      },
-      child: Container(
-        height: ScreenUtil().setHeight(100),
-        padding: EdgeInsets.only(left: 10, top: 20),
-        decoration: BoxDecoration(
-            color: isClick ? Color.fromRGBO(236, 236, 236, 1.0) : Colors.white,
-            border:
-                Border(bottom: BorderSide(width: 1, color: Colors.black12))),
-        child: Text(
-          list[index].mallCategoryName,
-          style: TextStyle(fontSize: ScreenUtil().setSp(30)),
-        ),
-      ),
-    );
-  }
-
-  void _getCategory() async {
-    await request('getCategory').then((val) {
-      var data = json.decode(val.toString());
-      print('------------分类-----------------');
-      CategoryModel category = CategoryModel.fromJson(data);
-      //list.data.forEach((item)=>print(item.mallCategoryName));
-      setState(() {
-        list = category.data;
-      });
-      Provide.value<ChildCategory>(context).getChildCategory(list[0].bxMallSubDto);
-    });
   }
 }
 
