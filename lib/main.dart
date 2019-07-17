9b3840e5168baa3f6e54376cfb89f4ad80824941
wangzhongjie <uvdream@163.com>
2019-07-17 16:24:25 +0800

    [feat] 分类右侧顶部

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,28 @@ import 'package:flutter/material.dart';
 import './pages/index_page.dart';
 import 'package:provide/provide.dart';
 import './provide/counter.dart';
-void main(){
-  var counter=Counter();
-  var providers=Providers();
-  providers..provide(Provider<Counter>.value(counter));
-  runApp(ProviderNode(child: MyApp(),providers: providers) );
+import './provide/child_category.dart';
+
+void main() {
+  var counter = Counter();
+  var childCategory=ChildCategory();
+  var providers = Providers();
+  providers
+  ..provide(Provider<Counter>.value(counter))
+  ..provide(Provider<ChildCategory>.value(childCategory));
+  
+  runApp(ProviderNode(child: MyApp(), providers: providers));
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       child: MaterialApp(
-        title:"百姓生活",
+        title: "百姓生活",
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: Colors.pink
-        ),
-        home:IndexPage(),
+        theme: ThemeData(primaryColor: Colors.pink),
+        home: IndexPage(),
       ),
     );
   }
--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -4,6 +4,8 @@ import '../service/service_method.dart';
 import 'dart:convert';
 import '../model/category.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provide/provide.dart';
+import '../provide/child_category.dart';
 
 class CategoryPage extends StatefulWidget {
   @override
@@ -14,20 +16,22 @@ class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("商品分类"),),
+      appBar: AppBar(
+        title: Text("商品分类"),
+      ),
       body: Container(
         child: Row(
           children: <Widget>[
-            LeftCatgegoryNav()
+            LeftCatgegoryNav(),
+            Column(
+              children: <Widget>[RightCategory()],
+            )
           ],
         ),
       ),
     );
   }
-  
-
 }
-
 
 // 左侧分类
 class LeftCatgegoryNav extends StatefulWidget {
@@ -36,56 +40,112 @@ class LeftCatgegoryNav extends StatefulWidget {
 }
 
 class _LeftCatgegoryNavState extends State<LeftCatgegoryNav> {
+  List list = [];
+  var listIndex = 0;
 
-  List list=[];
   @override
   void initState() {
     super.initState();
     _getCategory();
   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: ScreenUtil().setWidth(180),
       decoration: BoxDecoration(
-        border: Border(
-          right: BorderSide(width: 1,color: Colors.black12)
-        ),
+        border: Border(right: BorderSide(width: 1, color: Colors.black12)),
       ),
       child: ListView.builder(
         itemCount: list.length,
-        itemBuilder: (context,index){
+        itemBuilder: (context, index) {
           return _leftInkWell(index);
         },
       ),
     );
   }
-  Widget _leftInkWell(int index){
+
+  // 左侧子项
+  Widget _leftInkWell(int index) {
+    bool isClick = false;
+    isClick = (index == listIndex) ? true : false;
     return InkWell(
-      onTap: (){},
+      onTap: () {
+        setState(() {
+          listIndex = index;
+        });
+        var childList = list[index].bxMallSubDto;
+        Provide.value<ChildCategory>(context).getChildCategory(childList);
+      },
       child: Container(
         height: ScreenUtil().setHeight(100),
-        padding: EdgeInsets.only(left: 10,top: 20),
+        padding: EdgeInsets.only(left: 10, top: 20),
         decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border(
-            bottom: BorderSide(width: 1,color: Colors.black12)
-          )
+            color: isClick ? Colors.black26 : Colors.white,
+            border:
+                Border(bottom: BorderSide(width: 1, color: Colors.black12))),
+        child: Text(
+          list[index].mallCategoryName,
+          style: TextStyle(fontSize: ScreenUtil().setSp(30)),
         ),
-        child: Text(list[index].mallCategoryName,style: TextStyle(fontSize: ScreenUtil().setSp(30)),),
       ),
     );
   }
 
-  void _getCategory() async{
-    await request('getCategory').then((val){
-      var data=json.decode(val.toString());
+  void _getCategory() async {
+    await request('getCategory').then((val) {
+      var data = json.decode(val.toString());
       print('------------分类-----------------');
-      CategoryModel category=CategoryModel.fromJson(data);
+      CategoryModel category = CategoryModel.fromJson(data);
       //list.data.forEach((item)=>print(item.mallCategoryName));
       setState(() {
-        list=category.data;
+        list = category.data;
       });
     });
+  }
+}
+
+// 右侧
+class RightCategory extends StatefulWidget {
+  @override
+  _RightCategoryState createState() => _RightCategoryState();
+}
+
+class _RightCategoryState extends State<RightCategory> {
+  // List list=['名酒','宝丰','北京二锅头','茅台','五粮液','江小白'];
+  @override
+  Widget build(BuildContext context) {
+    return Provide<ChildCategory>(
+      builder: (context, child, childCategory) {
+        return Container(
+          height: ScreenUtil().setHeight(80),
+          width: ScreenUtil().setWidth(570),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border:
+                  Border(bottom: BorderSide(width: 1, color: Colors.black12))),
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: childCategory.childCategoryList.length,
+            itemBuilder: (context, index) {
+              return _rightInkwell(childCategory.childCategoryList[index]);
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _rightInkwell(BxMallSubDto item) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        padding: EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 10.0),
+        child: Text(
+          item.mallSubName,
+          style: TextStyle(fontSize: ScreenUtil().setSp(28)),
+        ),
+      ),
+    );
   }
 }
