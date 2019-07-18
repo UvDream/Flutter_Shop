33c9a00ba422a3da131e3dc942d7fd442d6c57eb
wangzhongjie <uvdream@163.com>
2019-07-18 19:15:28 +0800

    [fix] 格式

--- lib/pages/category/right_content.dart
+++ lib/pages/category/right_content.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -14,13 +13,14 @@ class CategoryGoodsList extends StatefulWidget {
 }
 
 class _CategoryGoodsListState extends State<CategoryGoodsList> {
-List list=[];
+  List list = [];
 
   @override
   void initState() {
     super.initState();
     _getGoodsLIst();
   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -28,38 +28,36 @@ List list=[];
       height: ScreenUtil().setHeight(985),
       child: ListView.builder(
         itemCount: list.length,
-        itemBuilder: (context,index){
+        itemBuilder: (context, index) {
           return _listWidget(index);
         },
       ),
     );
   }
-  void _getGoodsLIst() async{
-    var data={
-      'categoryId':'4',
-      'categorySubId':'',
-      'page':1
-    };
-    await request('getMallGoods',formData: data).then((val){
-      var data=json.decode(val.toString());
-      CategoryGoodsListModel goodsList=CategoryGoodsListModel.fromJson(data);
+
+  void _getGoodsLIst() async {
+    var data = {'categoryId': '4', 'categorySubId': '', 'page': 1};
+    await request('getMallGoods', formData: data).then((val) {
+      var data = json.decode(val.toString());
+      CategoryGoodsListModel goodsList = CategoryGoodsListModel.fromJson(data);
       // print('++++++++++商品内容+++++++++');
       // print(goodsList);
       setState(() {
-        list=goodsList.data;
+        list = goodsList.data;
       });
     });
   }
 
   // 商品图片
-  Widget _goodsImage(index){
+  Widget _goodsImage(index) {
     return Container(
       width: ScreenUtil().setWidth(200),
       child: Image.network(list[index].image),
     );
   }
+
   // 商品名
-  Widget _goodsName(index){
+  Widget _goodsName(index) {
     return Container(
       padding: EdgeInsets.all(5.0),
       width: ScreenUtil().setWidth(370),
@@ -71,44 +69,48 @@ List list=[];
       ),
     );
   }
+
   // 商品价格
-  Widget _goodsPrice(index){
+  Widget _goodsPrice(index) {
     return Container(
       width: ScreenUtil().setWidth(370),
       margin: EdgeInsets.only(top: 20),
       child: Row(
         children: <Widget>[
-          Text('价格:¥${list[index].presentPrice}',style: TextStyle(color: Colors.pink,fontSize: ScreenUtil().setSp(30)),),
-          Text('${list[index].oriPrice}',style: TextStyle(color: Colors.black26,decoration: TextDecoration.lineThrough),)
+          Text(
+            '价格:¥${list[index].presentPrice}',
+            style:
+                TextStyle(color: Colors.pink, fontSize: ScreenUtil().setSp(30)),
+          ),
+          Text(
+            '${list[index].oriPrice}',
+            style: TextStyle(
+                color: Colors.black26, decoration: TextDecoration.lineThrough),
+          )
         ],
       ),
     );
   }
+
   // 组合
-  Widget _listWidget(int index){
+  Widget _listWidget(int index) {
     return InkWell(
-      onTap: (){},
+      onTap: () {},
       child: Container(
-        padding: EdgeInsets.only(top: 5.0,bottom: 5.0),
+        padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
         decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border(
-            bottom: BorderSide(width: 1,color: Colors.black12)
-          )
-        ),
+            color: Colors.white,
+            border:
+                Border(bottom: BorderSide(width: 1, color: Colors.black12))),
         child: Row(
           children: <Widget>[
             _goodsImage(index),
             Column(
-              children: <Widget>[
-                _goodsName(index),
-                _goodsPrice(index)
-              ],
+              children: <Widget>[_goodsName(index), _goodsPrice(index)],
             )
           ],
         ),
       ),
     );
   }
-
 }
