f88f3604522f15d14b686a9d6f8e1d79f16cae7b
wangzhongjie <uvdream@163.com>
2019-07-18 10:41:13 +0800

    [feat]商品分类接口

--- lib/pages/category/right_content.dart
+++ lib/pages/category/right_content.dart
@@ -1,39 +1,7 @@
+
 import 'package:flutter/material.dart';
-import '../service/service_method.dart';
-import './category/left_nav.dart';
-import './category/right_top.dart';
-// json
 import 'dart:convert';
-
-
-class CategoryPage extends StatefulWidget {
-  @override
-  _CategoryPageState createState() => _CategoryPageState();
-}
-
-class _CategoryPageState extends State<CategoryPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("商品分类"),
-      ),
-      body: Container(
-        child: Row(
-          children: <Widget>[
-            LeftCatgegoryNav(),
-            Column(
-              children: <Widget>[
-                  RightCategory(),
-                  CategoryGoodsList()
-                ],
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
+import '../../service/service_method.dart';
 
 // 商品列表
 class CategoryGoodsList extends StatefulWidget {
@@ -67,4 +35,3 @@ class _CategoryGoodsListState extends State<CategoryGoodsList> {
 
 
 }
-
