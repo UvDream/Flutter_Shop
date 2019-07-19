a089fbc5f884fa3aed70208c2890eb410c1bd49f
wangzhongjie <uvdream@163.com>
2019-07-19 17:29:49 +0800

    [fix] 删除打印

--- lib/pages/category/right_content.dart
+++ lib/pages/category/right_content.dart
@@ -84,7 +84,6 @@ class _CategoryGoodsListState extends State<CategoryGoodsList> {
       'categorySubId': Provide.value<ChildCategory>(context).subId,
       'page': Provide.value<ChildCategory>(context).page
     };
-    print(data);
     await request('getMallGoods', formData: data).then((val) {
       var data = json.decode(val.toString());
       CategoryGoodsListModel goodsList = CategoryGoodsListModel.fromJson(data);
