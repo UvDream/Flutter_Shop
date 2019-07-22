41f6ea3b8a29710308259897db65a715e8931622
wangzhongjie <uvdream@163.com>
2019-07-22 17:30:44 +0800

    [fix] 说明

--- lib/pages/details/details_explain.dart
+++ lib/pages/details/details_explain.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provide/provide.dart';
-import '../../provide/details_info.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 // 说明栏
