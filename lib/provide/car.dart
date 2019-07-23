9c5e6f740f3efa29dbc7ce9b53d4105622894af6
wangzhongjie <uvdream@163.com>
2019-07-23 17:07:48 +0800

    [feat] 购物车model化

--- lib/provide/car.dart
+++ lib/provide/car.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
+import '../model/car_info.dart';
 
 class CarProvide with ChangeNotifier {
   String carString = "[]";
+
+  List<CarInfoModel> carList = [];
+// 保存
   save(goodsId, goodsName, count, price, images) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     carString = prefs.getString('carInfo');
@@ -14,30 +18,53 @@ class CarProvide with ChangeNotifier {
     tempList.forEach((item) {
       if (item['goodsId'] == goodsId) {
         tempList[ival]['count'] = item['count'] + 1;
+        carList[ival].count++;
         isHave = true;
       }
       ival++;
     });
     if (!isHave) {
-      tempList.add({
+      Map<String, dynamic> newGoods = {
         'goodsId': goodsId,
         'goodsName': goodsName,
         'count': count,
         'price': price,
         'images': images
-      });
+      };
+      tempList.add(newGoods);
+      carList.add(CarInfoModel.fromJson(newGoods));
     }
 
     carString = json.encode(tempList).toString();
-    print(carString);
+    print('字符串' + carString);
+    print('数据模型${carList}');
     prefs.setString('carInfo', carString);
     notifyListeners();
   }
 
+// 删除
   remove() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
+    // 清空数据模型
+    carList = [];
     prefs.remove('carInfo');
     print('清空完成---');
+    notifyListeners();
+  }
+
+  // 获取
+  getCarInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    carString = prefs.getString('carInfo');
+    carList = [];
+    if (carString == null) {
+      carList = [];
+    } else {
+      List<Map> tempList = (json.decode(carString.toString()) as List).cast();
+      tempList.forEach((item) {
+        carList.add(CarInfoModel.fromJson(item));
+      });
+    }
     notifyListeners();
   }
 }
